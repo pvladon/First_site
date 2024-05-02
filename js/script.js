91d46f0f0834d78abe530d74e0c1e6fc1b74a471
pvladon <pe4ersky.vlad@yandex.ru>
2024-05-02 12:39:19 +0300

    Исправил опечатку

--- js/script.js
+++ js/script.js
@@ -2,7 +2,7 @@ var count = 0;
 document.getElementById("myButton").onclick = function() {
 	count++;
 	if (count % 2 == 0) { 
-        document.getElemetnById("demo").innerHTML = "" 
+        document.getElementById("demo").innerHTML = "" 
 	} else {
         var img = document.createElement("img");
         img.src = "https://u.9111s.ru/uploads/202203/28/14f3eb7ac7e02a10e7314f70ede6ccbc.jpg"
